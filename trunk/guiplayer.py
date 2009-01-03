0f6995b10d3ec4bd53595ea88773a7f594c13e4f
sneakypete81 <sneakypete81@14155720-b5bb-11dd-bba1-afcbec1a1e1f>
2009-01-03 21:29:54 +0000

    Downloads tab working

--- trunk/guiplayer.py
+++ trunk/guiplayer.py
@@ -2,20 +2,22 @@
 
 # Todo:
 #  * Download manager
+#  * Allow deleted programmes to be reshown / undeleted
 #  * Thumbnails (cache?)
 #  * Highlight featured programmes
 #  * Configurable download location
 #  * Auto-install / update get_iplayer
 
-from __future__ import with_statement
 import subprocess
 import tempfile
 import pickle
+import copy
 import os
 import sys
 import urllib
 import cStringIO
 import wx
+import wx.lib.mixins.listctrl as listmix
 
 class Series():
     pass
@@ -38,16 +40,19 @@ class IPlayer():
 
     download_dir = "/home/pete/Movies"
 
-    def __init__(self):
+    def __init__(self, log):
+        self.log = log
         self._refresh_cache()
         self._parse_cache()
         self.ignored_programmes = set()
         self.downloaded_episodes = set()
+        self.log.write("Reading options file...")
         try:
             file = open(self.OPTIONS_FILE, "r")
             self.ignored_programmes = pickle.load(file)
             self.downloaded_episodes = pickle.load(file)
-        except:
+        except Exception, inst:
+            self.log.write("Error reading options file: %s" % str(inst))
             pass
 
     def __del__(self):
@@ -55,41 +60,47 @@ class IPlayer():
         pickle.dump(self.ignored_programmes, file)
         pickle.dump(self.downloaded_episodes, file)
 
-    def download(self, episode):
-        result = subprocess.check_call("get_iplayer --output %s --get %s" % 
-                                       (self.download_dir, episode.Pid), 
-                                       shell=True)
-        self.mark_as_downloaded(episode)
-
     def mark_as_downloaded(self, episode):
+        self.log.write("Marking episode %s as downloaded..." % episode.Pid)
         self.downloaded_episodes.add(episode.Pid)
 
     def _refresh_cache(self):
+        self.log.write("Refreshing cache...")
         result = subprocess.check_call("get_iplayer --quiet", shell=True)
 
     def _parse_cache(self):
+        self.log.write("Parsing cache...")
         self.programmes = {}
-        with open(self.TV_CACHE_FILE) as f:
-            # Parse the headings
-            first_line = f.readline().strip()
-            if first_line[0] != "#":
-                raise ValueError("Invalid cache file format")
-            headings = first_line[1:].split("|")
-            
-            for line in f:
-                # Create an episode object
-                episode = Episode()
-                split_line = line.split("|")
-                for i,heading in enumerate(headings):
-                    episode.__dict__[heading] = split_line[i]
+        f = open(self.TV_CACHE_FILE)
+        # Parse the headings
+        first_line = f.readline().strip()
+        if first_line[0] != "#":
+            self.log.write("Invalid cache file format.")
+            self.log.write("First line was: '%s'" % first_line)
+            raise ValueError("Invalid cache file format.")
 
-                episode.Name = unicode(episode.Name, 'utf-8')
+        headings = first_line[1:].split("|")       
+        self.log.write("Cache headings: %s" % str(headings))
 
-                # Create a new programme if necessary
-                if episode.Name not in self.programmes:
-                    self.programmes[episode.Name] = []
-                    
-                self.programmes[episode.Name].append(episode)
+        lines = f.readlines()
+        self.log.write("Parsing %d lines from cache..." % len(lines))
+
+        for line in lines:
+            # Create an episode object
+            episode = Episode()
+            split_line = line.split("|")
+            for i,heading in enumerate(headings):
+                episode.__dict__[heading] = split_line[i]
+
+            episode.Name = unicode(episode.Name, 'utf-8')
+
+            # Create a new programme if necessary
+            if episode.Name not in self.programmes:
+                self.programmes[episode.Name] = []
+                
+            self.programmes[episode.Name].append(episode)
+
+        self.log.write("Cache parsing complete.")
 
 class IPlayerFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
@@ -100,15 +111,44 @@ class IPlayerFrame(wx.Frame):
 class IPlayerPanel(wx.Panel):
     def __init__(self, *args, **kwargs):
         wx.Panel.__init__(self, *args, **kwargs) 
-        self.iplayer = IPlayer()
-        self._make_widgets()
 
-        self.programme_list.populate()
+        nb = wx.Notebook(self, style=wx.NB_TOP) 
 
-    def _make_widgets(self):
+        self.log = LogTab(nb)
+        iplayer = IPlayer(self.log)
+
+        self.programme_tab = ProgrammeTab(nb, 
+                                iplayer, 
+                                download_callback=self._download_episode,
+                                log=self.log)
+        self.download_tab = DownloadTab(nb, 
+                                iplayer, 
+                                refresh_callback=self._refresh_programme_tab,
+                                log=self.log)
+
+        nb.AddPage(self.programme_tab, "Programmes")
+        nb.AddPage(self.download_tab, "Downloads")
+        nb.AddPage(self.log, "Log")
+
+        topbox = wx.BoxSizer(wx.VERTICAL)
+        topbox.Add(nb, proportion=1, flag=wx.EXPAND)
+        self.SetSizerAndFit(topbox)
+
+    def _download_episode(self, episode):
+        self.download_tab.add(episode)
+
+    def _refresh_programme_tab(self):
+        self.programme_tab.refresh()
+        
+class ProgrammeTab(wx.Panel):
+    def __init__(self, parent, iplayer, download_callback, log):
+        wx.Panel.__init__(self, parent) 
+        self.iplayer = iplayer
+        self.log = log
+        
         hbox = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.programme_list = ProgrammeList(self, self.iplayer)
+        self.programme_list = ProgrammeList(self, self.iplayer, log=self.log)
         self.programme_list.list.Bind(wx.EVT_LIST_ITEM_SELECTED, 
                                       self._programme_change,
                                       self.programme_list.list)
@@ -116,8 +156,8 @@ class IPlayerPanel(wx.Panel):
 
         vbox = wx.BoxSizer(wx.VERTICAL)
 
-        label = wx.StaticText(self, label="Episodes:")
-        vbox.Add(label)
+#        label = wx.StaticText(self, label="Episodes:")
+#        vbox.Add(label)
 
         self.episode_list = EpisodeList(self, self.iplayer, self._refresh_programme_list)
         self.episode_list.Bind(wx.EVT_LIST_ITEM_SELECTED, 
@@ -125,11 +165,15 @@ class IPlayerPanel(wx.Panel):
                                self.episode_list)
         vbox.Add(self.episode_list, proportion=1, flag=wx.EXPAND)
 
-        self.episode_panel = EpisodePanel(self, self.iplayer, self._refresh_episode_list)
+        self.episode_panel = EpisodePanel(self, self.iplayer, 
+                             refresh_callback=self._refresh_episode_list,
+                             download_callback=download_callback)
         vbox.Add(self.episode_panel, proportion=1, flag=wx.EXPAND)
 
         hbox.Add(vbox, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
         self.SetSizerAndFit(hbox)
+
+        self.programme_list.populate()
 
     def _programme_change(self, event):
         programme_name = event.GetText()
@@ -142,25 +186,30 @@ class IPlayerPanel(wx.Panel):
             episode = self.episode_list.programme[index]
             self.episode_panel.populate(episode)
 
+    def refresh(self):
+        self._refresh_episode_list()
+        self._refresh_programme_list()
+
+    def _refresh_programme_list(self):
+        self.programme_list.refresh()
+
     def _refresh_episode_list(self):
         self.episode_list.refresh()
  
-    def _refresh_programme_list(self):
-        self.programme_list.refresh()
-               
-
 class ProgrammeList(wx.Panel):
     DOWNLOADED_ITEM_COLOUR = wx.LIGHT_GREY
 
-    def __init__(self, parent, iplayer):  
-        wx.Panel.__init__(self, parent)
+    def __init__(self, parent, iplayer, log):  
         self.iplayer = iplayer
-        
+        self.log = log
+
+        wx.Panel.__init__(self, parent)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
         self.list = wx.ListCtrl(self, style=wx.LC_REPORT |
                                             wx.LC_SINGLE_SEL |
-                                            wx.LC_NO_HEADER)
+                                            wx.LC_NO_HEADER |
+                                            wx.BORDER_SUNKEN)
         self.list.InsertColumn(0, "")
         self.list.Bind(wx.EVT_CHAR, self._on_key)
         vbox.Add(self.list, proportion=1, flag=wx.EXPAND)
@@ -209,7 +258,9 @@ class ProgrammeList(wx.Panel):
         if item == -1:
             return
 
-        self.iplayer.ignored_programmes.add(self.list.GetItemText(item))
+        programme_name = self.list.GetItemText(item)
+        self.log.write("Deleting programme %s..." % programme_name)
+        self.iplayer.ignored_programmes.add(programme_name)
         self.list.DeleteItem(item)
 
         # Select the next item automatically
@@ -231,7 +282,8 @@ class EpisodeList(wx.ListCtrl):
         wx.ListCtrl.__init__(self, parent, 
                              style=wx.LC_REPORT | 
                                    wx.LC_SINGLE_SEL |
-                                   wx.LC_NO_HEADER)
+                                   wx.LC_NO_HEADER |
+                                   wx.BORDER_SUNKEN)
         self.InsertColumn(0, "")
 
     def populate(self, programme):
@@ -258,9 +310,10 @@ class EpisodeList(wx.ListCtrl):
             self.SetItemState(next_item, wx.LIST_STATE_SELECTED, wx.LIST_STATE_SELECTED)
 
 class EpisodePanel(wx.Panel):
-    def __init__(self, parent, iplayer, refresh_callback):
+    def __init__(self, parent, iplayer, refresh_callback, download_callback):
         self.iplayer = iplayer
         self.refresh_callback = refresh_callback
+        self.download_callback = download_callback
         self.episode = None
         wx.Panel.__init__(self, parent)
         
@@ -302,13 +355,229 @@ class EpisodePanel(wx.Panel):
 #        self.thumbnail.SetBitmap(bmp)
 
     def download(self, event):
-        self.iplayer.download(self.episode)
-        pass
+        if self.episode is not None:
+            self.download_callback(self.episode)
+#        self.iplayer.download(self.episode)
 
     def mark_as_downloaded(self, event):
         self.iplayer.mark_as_downloaded(self.episode)
         self.refresh_callback()
 
+
+class DownloadTab(wx.Panel):
+    def __init__(self, parent, iplayer, log, refresh_callback):
+        self.iplayer = iplayer
+        self.log = log
+        wx.Panel.__init__(self, parent)
+
+        topbox = wx.BoxSizer(wx.VERTICAL)
+        self.list = DownloadList(self, self.iplayer, log=self.log,
+                                 refresh_callback=refresh_callback)
+        topbox.Add(self.list, 
+                   proportion=1, 
+                   border=5,
+                   flag=wx.EXPAND | wx.ALL)
+
+        self.SetSizerAndFit(topbox)
+
+    def add(self, episode):
+        self.list.add(episode)
+
+class DownloadList(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
+    PROCESS_READ_INTERVAL = 200 # suck on the pipe every 200ms
+    
+    def __init__(self, parent, iplayer, log, refresh_callback):
+        self.iplayer = iplayer
+        self.log = log
+        self.refresh_callback = refresh_callback
+        self.current_item = None
+        self.episodes = {}
+        wx.ListCtrl.__init__(self, parent, 
+                             style=wx.LC_REPORT | 
+                                   wx.LC_SINGLE_SEL |
+                                   wx.BORDER_SUNKEN)
+        listmix.ListCtrlAutoWidthMixin.__init__(self)
+
+        self.InsertColumn(0, "Episode")
+        self.COL_EPISODE = 0
+        self.InsertColumn(1, "Status")
+        self.COL_STATUS = 1
+        self.InsertColumn(2, "Size")
+        self.COL_SIZE = 2
+        self.InsertColumn(3, "Progress")
+        self.COL_PROGRESS = 3
+        self.InsertColumn(4, "Speed")
+        self.COL_SPEED = 4
+        self.InsertColumn(5, "Remaining Time")
+        self.COL_REMAINING_TIME = 5
+        self.size_columns()
+
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self._on_timer)
+        self.process = None
+        self.Bind(wx.EVT_END_PROCESS, self._on_process_ended)
+
+    def __del__(self):
+        if self.process is not None:
+            self.process.Detach()
+            self.process.CloseOutput()
+            self.process = None
+            self.timer.Stop()
+
+    def size_columns(self):
+        self.InsertStringItem(self.COL_EPISODE, "Hello")
+        self.SetStringItem(0, self.COL_STATUS, "Downloading")
+        self.SetColumnWidth(self.COL_STATUS, wx.LIST_AUTOSIZE)
+        self.SetStringItem(0, self.COL_SIZE, "888.88MB / 888.88MB")
+        self.SetColumnWidth(self.COL_SIZE, wx.LIST_AUTOSIZE)
+        self.SetStringItem(0, self.COL_PROGRESS, "100%")
+        self.SetColumnWidth(self.COL_PROGRESS, wx.LIST_AUTOSIZE_USEHEADER)
+        self.SetStringItem(0, self.COL_SPEED, "88888kbps")
+        self.SetColumnWidth(self.COL_SPEED, wx.LIST_AUTOSIZE)
+        self.SetStringItem(0, self.COL_REMAINING_TIME, "88:88:88")
+        self.SetColumnWidth(self.COL_REMAINING_TIME, wx.LIST_AUTOSIZE_USEHEADER)
+        self.setResizeColumn(self.COL_EPISODE+1) # +1 due to wxPython bug
+        self.DeleteItem(0)
+
+    def add(self, episode):
+        self.log.write("Adding episode %s to the download queue." % episode.Pid)
+        item = self.InsertStringItem(sys.maxint, episode.Episode)
+
+        # Take a copy, so we can handle multiple instances of the same episode
+        self.episodes[item] = copy.copy(episode)
+        episode = self.episodes[item]
+        
+        episode.queued = True
+        episode.error = False
+        episode.error_message = ""
+        self.SetStringItem(item, self.COL_STATUS, "Queued")
+        self.episodes[item] = episode
+
+        if self.current_item is None:
+            self.download_next()
+
+    def download_next(self):
+        queued_episodes = [item for (item, episode) in self.episodes.items()
+                            if episode.queued]
+        if queued_episodes == []:
+            self.log.write("No more episodes in download queue.")
+            return
+
+        self.download(queued_episodes[0])
+
+    def download(self, item):
+        episode = self.episodes[item]
+        self.SetStringItem(item, self.COL_STATUS, "Downloading")
+        self.current_item = item
+        self.log.write("Downloading episode %s..." % episode.Pid)
+        cmd = "get_iplayer --output %s --get %s" % (self.iplayer.download_dir, 
+                                                    episode.Pid)
+        self.process = wx.Process(self)
+        self.process.Redirect();
+        result = wx.Execute(cmd, wx.EXEC_ASYNC, self.process)
+        if result <= 0:
+            episode.error = True
+            episode.error_message = ("Could not execute get_iplayer " +
+                                     "(error code %d)" % result)
+            self.log.error(error_message)
+            self._on_process_ended()
+            return
+            
+        self.stream = self.process.GetInputStream()
+        self.data = ""
+        self.timer.Start(self.PROCESS_READ_INTERVAL)
+        self.log.write("Started get_iplayer process.")
+
+        
+    def read(self):
+        if self.stream.CanRead():
+            data = self.stream.read()
+            data = self.data + data
+
+            # Split at \n's and \r's
+            lines = data.split("\n")
+            items = []
+            for line in lines:
+                items += line.split("\r")
+
+            while len(items) > 1:
+                self._process_line(items.pop(0).strip())
+            self.data = items[0]
+
+
+    def _process_line(self, line):
+        self.log.write("get-iplayer : %s" % line)
+        if line.startswith("ERROR:"):
+            error_message = line[6:].strip()
+            self.log.error(error_message)
+            self.episodes[self.current_item].error = True
+            self.episodes[self.current_item].error_message += error_message
+        try:
+            (downloaded, line) = line.split(" / ")
+            (size, rate, line) = line.split("  ", 2)
+            (percent, line) = line.split("%, ")
+            (remaining_time, line) = line.split(" remaining")
+            
+            self.SetStringItem(self.current_item, self.COL_SIZE, "%s / %s" % (downloaded, size))
+            self.SetStringItem(self.current_item, self.COL_PROGRESS, "%s%%" % percent)
+            self.SetStringItem(self.current_item, self.COL_SPEED, rate)
+            self.SetStringItem(self.current_item, self.COL_REMAINING_TIME, remaining_time)
+
+        except ValueError, msg:
+            # Couldn't understand the line - ignore
+            pass
+            
+    def _on_timer(self, event):
+        self.read()
+
+    def _on_process_ended(self, event):
+        episode = self.episodes[self.current_item]
+        self.log.write("get-iplayer process ended")
+        self.read()
+        self.process.Destroy()
+        self.process = None
+        self.timer.Stop()
+
+        self.SetStringItem(self.current_item, self.COL_STATUS, "Downloaded")
+        self.SetStringItem(self.current_item, self.COL_SIZE, "")
+        self.SetStringItem(self.current_item, self.COL_PROGRESS, "")
+        self.SetStringItem(self.current_item, self.COL_SPEED, "")
+        self.SetStringItem(self.current_item, self.COL_REMAINING_TIME, "")
+
+        if episode.error:
+            self.SetStringItem(self.current_item, self.COL_STATUS, "Error: %s" %
+                               episode.error_message)
+        else:
+            self.iplayer.mark_as_downloaded(episode)
+            self.refresh_callback()
+            
+        episode.queued = False
+        self.current_item = None
+        self.download_next()
+
+
+class LogTab(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+
+        topbox = wx.BoxSizer(wx.VERTICAL)
+        self.text = wx.TextCtrl(self, style=wx.TE_MULTILINE | 
+                                            wx.TE_READONLY |
+                                            wx.BORDER_SUNKEN)
+        topbox.Add(self.text, 
+                   proportion=1, 
+                   border=5,
+                   flag=wx.EXPAND | wx.ALL)
+
+        self.SetSizerAndFit(topbox)
+
+    def write(self, text):
+        self.text.AppendText("%s\n" % text)
+
+    def error(self, text):
+        self.text.AppendText("***********\n")
+        self.text.AppendText("*** ERROR : %s\n" % text)
+        self.text.AppendText("***********\n")
 
 def run():
     app = wx.App(redirect=False)
