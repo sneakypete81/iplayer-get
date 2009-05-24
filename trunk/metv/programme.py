c7380c94652c409ccc14c561ed698a7d983cc29f
sneakypete81 <sneakypete81@14155720-b5bb-11dd-bba1-afcbec1a1e1f>
2009-05-24 18:00:05 +0000

    started ignore/download

--- trunk/metv/programme.py
+++ trunk/metv/programme.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 class Programme():
-    def __init__(self, channel, name):
-        self.channel = channel
+    def __init__(self, channel_obj, name):
+        self.channel_obj = channel_obj
         self.name = name
         self.episodes = []
 
