3b1b31e4c2296775edd77d665ed3ef97e189e6c6
sneakypete81 <sneakypete81@14155720-b5bb-11dd-bba1-afcbec1a1e1f>
2009-07-12 17:54:31 +0000

    Use AuiNotebook. Add configure button

--- metv/view/ChannelToolbar.py
+++ metv/view/ChannelToolbar.py
@@ -4,19 +4,26 @@ import images
 class ChannelToolbar(wx.ToolBar):
     ID_SUBSCRIBE = 101
     ID_UNSUBSCRIBE = 102
+    ID_SETTINGS = 103
 
     def __init__(self, *args, **kwds):
         kwds["style"] = wx.TB_FLAT|wx.TB_TEXT
         wx.ToolBar.__init__(self, *args, **kwds)
+
         self.AddLabelTool(self.ID_SUBSCRIBE, 
                           label="Subscriptions", 
                           bitmap=images.getEpisodeSubscribeBitmap(),
-                          shortHelp="Manage programme subscriptions")
+                          shortHelp="Manage the programme subscriptions")
+        self.AddLabelTool(self.ID_SETTINGS, 
+                          label="Settings", 
+                          bitmap=images.getConfigureBitmap(),
+                          shortHelp="Modify the Idiot Box settings")
+        self.AddSeparator()
         self.AddLabelTool(self.ID_UNSUBSCRIBE, 
                           label="Hide", 
                           bitmap=images.getEditDeleteBitmap(),
-                          shortHelp="Remove selected programme from the list")
-
+                          shortHelp="Remove the selected programme from the list")
+ 
         self.__set_properties()
         self.__do_layout()
 
